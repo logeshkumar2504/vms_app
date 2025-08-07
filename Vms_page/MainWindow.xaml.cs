7d46a45e216aa3a01e1edcac6d0253b4a5d2226d
logeshkumar2504 <logesh@ofella.in>
2025-08-08 03:00:21 +0530

    drag funtionallty done

--- Vms_page/MainWindow.xaml.cs
+++ Vms_page/MainWindow.xaml.cs
@@ -1,39 +1,48 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace GuardStationUI
 {
+    public class MenuItemModel
+    {
+        public string Name { get; set; }
+        public string Group { get; set; } // "Basic" or "Smart"
+    }
+
     public partial class MainWindow : Window
     {
+        public ObservableCollection<MenuItemModel> BasicMenuItems { get; set; } = new();
+        public ObservableCollection<MenuItemModel> SmartMenuItems { get; set; } = new();
+        public ObservableCollection<MenuItemModel> DroppedItems { get; set; } = new();
         private bool isDarkMode = true;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void AccessControl_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Access Control Clicked");
-        }
-
-        private void LPR_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("LPR Clicked");
-        }
-
-        private void PeopleCounting_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("People Counting Clicked");
-        }
-
-        private void VCA_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("VCA Clicked");
-        }
-
-        private void FaceRecognition_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Face Recognition Clicked");
+            DataContext = this;
+            // Initialize menu items
+            BasicMenuItems.Add(new MenuItemModel { Name = "User Management", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Recording Schedule", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "View", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Alarm Records", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Device Management", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Alarm Configuration", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "System Configuration", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Sequence Resource", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Live View", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Operation Log", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Playback", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Audio", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Video Wall", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "E-map", Group = "Basic" });
+            BasicMenuItems.Add(new MenuItemModel { Name = "Recording Schdual", Group = "Basic" });
+            SmartMenuItems.Add(new MenuItemModel { Name = "Face Recognition", Group = "Smart" });
+            SmartMenuItems.Add(new MenuItemModel { Name = "Access Control", Group = "Smart" });
+            SmartMenuItems.Add(new MenuItemModel { Name = "LPR", Group = "Smart" });
+            SmartMenuItems.Add(new MenuItemModel { Name = "People Counting", Group = "Smart" });
+            SmartMenuItems.Add(new MenuItemModel { Name = "VCA", Group = "Smart" });
         }
 
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
@@ -54,22 +63,54 @@ namespace GuardStationUI
             Close();
         }
 
-        private void DarkModeButton_Click(object sender, RoutedEventArgs e)
+        private void MenuButton_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            isDarkMode = !isDarkMode;
-            ToggleDarkMode(isDarkMode);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var button = sender as Button;
+                if (button?.Tag is MenuItemModel item)
+                {
+                    DragDrop.DoDragDrop(button, item, DragDropEffects.Move);
+                }
+            }
         }
 
-        private void ToggleDarkMode(bool dark)
+        private void DropTargetGrid_DragOver(object sender, DragEventArgs e)
         {
-            if (dark)
-            {
-                this.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1B1E2B"));
-            }
+            if (e.Data.GetDataPresent(typeof(MenuItemModel)))
+                e.Effects = DragDropEffects.Move;
             else
+                e.Effects = DragDropEffects.None;
+            e.Handled = true;
+        }
+
+        private void DropTargetGrid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(MenuItemModel)))
             {
-                this.Background = new SolidColorBrush(Colors.WhiteSmoke);
+                var item = (MenuItemModel)e.Data.GetData(typeof(MenuItemModel));
+                if (!DroppedItems.Contains(item))
+                {
+                    DroppedItems.Add(item);
+                    if (item.Group == "Basic")
+                        BasicMenuItems.Remove(item);
+                    else if (item.Group == "Smart")
+                        SmartMenuItems.Remove(item);
+                }
+            }
+        }
+
+        private void DroppedItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Tag is MenuItemModel item)
+            {
+                DroppedItems.Remove(item);
+                if (item.Group == "Basic")
+                    BasicMenuItems.Add(item);
+                else if (item.Group == "Smart")
+                    SmartMenuItems.Add(item);
             }
         }
     }
 }
+// This code is part of a WPF application that implements a main window with buttons for various functionalities.
--- Vms_page/obj/Debug/net9.0-windows/Vms_page.AssemblyInfo.cs
+++ Vms_page/obj/Debug/net9.0-windows/Vms_page.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vms_page")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac22d09495f51defd5d6242523897066a0eb394")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949dd60072891de14aa821982b91142e09b93587")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vms_page")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vms_page")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
